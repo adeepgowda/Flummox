7fed180b9c1acb407dc4422660591a1365458ce7
adeepgowda <34350113+adeepgowda@users.noreply.github.com>
2018-06-17 20:03:22 +0530

    Update errorfunction.h

--- errorfunction.h
+++ errorfunction.h
@@ -9,70 +9,101 @@
 #ifndef errorfunction_h
 #define errorfunction_h
 
+//
+//Primary Error = Errors which are concerned with the actions performed pre-translation of languages. Ex.: File extension error, file not found, etc.
+//Secondary Error = Errors which are concerned with the pure mechanism of translation of languages. Ex.: Syntax errors, etc.
+//
+
+extern bool success;
+extern std::string cmd_global;
+
 void error (int errorcode, unsigned int b, unsigned int i)
 {
+    std::string cmd = cmd_global;
+    std::cout << std::endl << std::endl << "        Error " << errorcode << " : ";
+    
     switch (errorcode)
     {
-        case -1:
-            std::cout << std::endl << "Error -1 : File Extension Error !!!" << std::endl;
-            break;
-            
+        // Relating to all primary errors from this point on
         case 0:
-            std::cout << std::endl << "Error 0 : Define the co-ordinate parameter & the measurement parameter." << std::endl;
+            std::cout << "File Extension Error !!!";
             break;
             
         case 1000:
-            std::cout << std::endl << "Error 1000 : Syntax Error / Function Not Available." << std::endl;
+            std::cout << "Syntax Error / Function Not Available.";
+            break;
+            
+        case 1001:
+            std::cout << "Define the co-ordinate parameter & the measurement parameter.";
             break;
             
         case 2000:
-            std::cout << std::endl << "Error 2000 : Syntax Error, Available Functions - Define" << std::endl;
+            std::cout << "Syntax Error, Available Functions - Define";
             break;
         
         case 2001:
-            std::cout << std::endl << "Error 2001 : Syntax Error, Available Functions - Absloute, Incremental with IJK & Metric, Imperial Units" << std::endl;
+            std::cout << "Syntax Error, Available Functions - Absloute, Incremental with IJK & Metric, Imperial Units";
             break;
             
         case 2002:
-            std::cout << std::endl << "Error 2002 : Define the ABSOLUTE positioning function correctly." << std::endl;
+            std::cout << "Define the ABSOLUTE positioning function correctly.";
             break;
             
         case 2003:
-            std::cout << std::endl << "Error 2003 : Define the INCREMENTAL positioning function correctly." << std::endl;
+            std::cout << "Define the INCREMENTAL positioning function correctly.";
             break;
             
-        // Relating to all functions from letter 'c' this point on
+        // Relating to all functions from letter 'c' from this point on
             
         case 3000:
-            std::cout << std::endl << "Error 3000 : Syntax Error, Available Functions - Change" << std::endl;
+            std::cout << "Syntax Error, Available Functions - Change";
             break;
         
         case 3001:
-            std::cout << std::endl << "Error 3001 : Syntax Error, Available Functions - Speed, Tool, Coolant" << std::endl;
+            std::cout << "Syntax Error, Available Functions - Speed, Tool, Coolant";
             break;
             
         case 3002:
-            std::cout << std::endl << "Error 3002 : Define the SPEED function correctly." << std::endl;
+            std::cout << "Define the SPEED function correctly.";
             break;
             
         case 3003:
-            std::cout << std::endl << "Error 3003 : Define the TOOL function correctly." << std::endl;
+            std::cout << "Define the TOOL function correctly.";
             break;
             
         case 3004:
-            std::cout << std::endl << "Error 3004 : Define the COOLANT function correctly." << std::endl;
+            std::cout << "Define the COOLANT function correctly.";
             break;
         
         case 55555:
-            std::cout << std::endl << "Error 5555 : Experimental." << std::endl;
+            std::cout << "Experimental.";
             break;
         
         default:
-            std::cout << std::endl << "Error Code Not Recognized, Contact Dev." << std::endl;
+            std::cout << "Error Code Not Recognized, Contact Dev.";
             break;
     }
-    if((b!=0) && (i!=0))
-        std::cout << "At line "<< i+1 << " & at position " << b+1 << "." << std::endl;
+    
+    if((b!=0) && (i!=0))                // Check if the error made is primary or secondary and enter only if error is secondary
+    {
+        std::cout << std::endl << "        At line "<< i+1 << " & at position " << b+1 << "." << std::endl;
+        std::cout << std::endl << "        " << cmd << std::endl;
+        for (unsigned int e = 0; e!=(b+5); ++e)
+            std::cout << " ";
+        std::cout << "^" << std::endl;
+        for (unsigned int e = 0; e!=(b+5); ++e)
+            std::cout << " ";
+        std::cout << "Here" << std::endl;
+    }
+    if (errorcode >= 1000)
+        throw 1;
+    else
+    {
+        success=false;
+        throw 0;
+    }
 }
 
+
 #endif /*errorfunction_h */
+
